29fdfbc8f61fd5b470b82a16d081a1c3bce0cc6e
Damien Dillon <damien.dillon+github@gmail.com>
2017-12-14 18:44:09 +0100

    Finish Cloudinary

--- app/models/worker_profile.rb
+++ app/models/worker_profile.rb
@@ -1,12 +1,11 @@
 class WorkerProfile < ApplicationRecord
   belongs_to :user
-  validates :user_id, uniqueness: true
+  validates :user_id, presence: true
   SKILL_AREA = ["Builder", "Painter", "Carpenter", "Electrician", "Cleaner", "Handyman", "Other"]
   validates :skill_area, inclusion: {in: SKILL_AREA}
    # Added
   has_many :jobs, through: :requests
   has_many :requests
 
-  mount_uploader :photo, PhotoUploader
-
+ accepts_nested_attributes_for :user
 end
