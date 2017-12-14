8bab5abd421526ada193f43212e4a1289cb52f7e
Damien Dillon <damien.dillon+github@gmail.com>
2017-12-14 12:51:57 +0100

    Add worker skills as a set

--- app/models/worker_profile.rb
+++ app/models/worker_profile.rb
@@ -1,6 +1,8 @@
 class WorkerProfile < ApplicationRecord
   belongs_to :user
   validates :user_id, uniqueness: true
+  SKILL_AREA = ["Builder", "Painter", "Carpenter", "Electrician", "Cleaner", "Handyman", "Other"]
+  validates :skill_area, inclusion: {in: SKILL_AREA}
    # Added
   has_many :jobs, through: :requests
   has_many :requests
