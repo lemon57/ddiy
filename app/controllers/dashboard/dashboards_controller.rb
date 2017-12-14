ff429392ab20ab0dfd00744330496536666cc383
Roman Kislov <gzeek57@gmail.com>
2017-12-14 19:04:52 +0100

    navbar dashboards and dashboards controller

--- app/controllers/dashboard/dashboards_controller.rb
+++ app/controllers/dashboard/dashboards_controller.rb
@@ -1,5 +1,14 @@
 class Dashboard::DashboardsController < ApplicationController
   def worker
+
+    # @job = Job.last
+    @user = current_user
+    @jb_id = @user.worker_profile.user_id
+    @worker_profile = WorkerProfile.find("#{@user.worker_profile.id}")
+    @request = Request.where()
+    @job = Job.where(id: "@user.worker_profile.user_id")
+
+    raise
   end
 
   def owner
