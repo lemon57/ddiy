8a2e0edbf389a35c201a2606164b2263dd7d1954
Daniel Contreras <danicurri7603@gmail.com>
2017-12-15 14:58:42 +0100

    added form in worker request show page

--- app/controllers/dashboard/dashboards_controller.rb
+++ app/controllers/dashboard/dashboards_controller.rb
@@ -3,10 +3,10 @@ class Dashboard::DashboardsController < ApplicationController
 
     # @job = Job.last
     @user = current_user
-    @jb_id = @user.worker_profile.user_id
+    #@job_id = @user.worker_profile.user_id
     @worker_profile = WorkerProfile.find("#{@user.worker_profile.id}")
     # @request = Request.all.where(worker_profile_id: @worker_profile.id)[0]
-    @request = Request.find("#{@worker_profile.id}")
+    @requests = Request.where(worker_profile_id: @worker_profile.id)
     # @job = Job.all.where(id: @request.job_id)
     @job = Job.find("#{@request.job_id}")
 
