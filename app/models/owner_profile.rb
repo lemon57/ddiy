654af7dee70195bda3180476a3986e3dc8bd1bf1
Damien Dillon <damien.dillon+github@gmail.com>
2017-12-18 17:00:49 +0100

    Add capacity to edit profiles

--- app/models/owner_profile.rb
+++ app/models/owner_profile.rb
@@ -1,6 +1,6 @@
 class OwnerProfile < ApplicationRecord
   belongs_to :user
-  validates :user_id, presence: true
+  validates :user_id, uniqueness: true
   # Added
   has_many :jobs
 
--- app/models/worker_profile.rb
+++ app/models/worker_profile.rb
@@ -1,6 +1,6 @@
 class WorkerProfile < ApplicationRecord
   belongs_to :user
-  validates :user_id, presence: true
+  validates :user_id, uniqueness: true
   SKILL_AREA = ["Builder", "Painter", "Carpenter", "Electrician", "Cleaner", "Handyman", "Other"]
   validates :skill_area, inclusion: {in: SKILL_AREA}
    # Added
