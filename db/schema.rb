ffd4cbea605894cd327067e84a94a7b47179fb15
Damien Dillon <damien.dillon+github@gmail.com>
2017-12-19 21:36:02 +0100

    fix db error

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20171219152803) do
     t.datetime "updated_at",       null: false
     t.string   "title"
     t.string   "description"
-    t.string   "photos"
+    t.json     "photos"
     t.string   "category"
     t.index ["owner_profile_id"], name: "index_jobs_on_owner_profile_id", using: :btree
   end
@@ -76,11 +76,8 @@ ActiveRecord::Schema.define(version: 20171219152803) do
     t.time     "start_time"
     t.float    "material_cost",     default: 0.0
     t.datetime "time"
-
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-    t.integer  "price_cents",       default: 0, null: false
-
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.index ["job_id"], name: "index_requests_on_job_id", using: :btree
     t.index ["worker_profile_id"], name: "index_requests_on_worker_profile_id", using: :btree
   end
