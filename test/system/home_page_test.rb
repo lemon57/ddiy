d39f4cea5710c13eebc6c6bf33a147a36bc031bd
Damien Dillon <damien.dillon+github@gmail.com>
2017-12-20 17:23:41 +0100

    NOT COMPLETE DO NO PULL Test setup

--- test/system/home_page_test.rb
+++ test/system/home_page_test.rb
@@ -1,6 +1,6 @@
 require "application_system_test_case"
 
-class ProductsTest < ApplicationSystemTestCase
+class HomePageTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit "/"
     assert_selector "h1", text: "Book local professionals for repair and renovations"
