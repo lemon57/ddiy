cd64b1ec62f410bf00bb9bdcc9f38e5e313c380f
Roman Kislov <gzeek57@gmail.com>
2017-12-21 15:52:18 +0100

    fix configuration issue in application rb

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,10 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.serve_static_assets = true
 
 module Ddiy
   class Application < Rails::Application
+config.serve_static_assets = true
     config.generators do |generate|
       generate.assets false
       generate.helper false
